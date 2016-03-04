604ef8765872c7a0c6f2fe6a6cbea5bc0f3e1656
No One <No.Ones.Email@gmail.com>
2016-03-03 20:02:04 -0800

    backpack
    sponge bob jeopardy

---
+++ src/SpongeBobrun.java
@@ -0,0 +1,18 @@
+
+public class SpongeBobrun {public static void main(String[] args) {
+	/*
+	 * 1. In a Runner class, make Spongebob. Use the methods below to make him eat, and laugh.
+	 * 2. Make Patrick and Squidward and print their name, have them eat, and make them laugh.
+	 */
+SpongeBob mybob = new SpongeBob("SpongeBob");
+SpongeBob mypat = new SpongeBob("Patrick");
+SpongeBob mysquid = new SpongeBob("Squidward");
+mybob.eat();
+mybob.laugh();
+mypat.eat();
+mypat.laugh();
+mysquid.eat();
+mysquid.laugh();
+}
+
+}
